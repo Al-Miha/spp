82c81bef0446a155f87b53b6713ffc6628969ae5
Al Miha <contact.almiha@gmail.com>
2025-03-10 18:45:18 +0100

    125.AxiosInstance

--- frontend-react/src/components/dashboard/Dashboard.jsx
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -1,19 +1,17 @@
 import { useEffect } from "react";
 import axios from "axios";
+import axiosInstance from "../../axiosinstance";
 
 const Dashboard = () => {
   const accessToken = localStorage.getItem("accessToken");
   useEffect(() => {
     const fetchProtectedData = async () => {
       try {
-        const response = await axios.get(
-          "http://127.0.0.1:8000/api/v1/protected-view",
-          {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-          }
-        );
+        const response = await axiosInstance.get("/protected-view", {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
         console.log("Success: ", response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
