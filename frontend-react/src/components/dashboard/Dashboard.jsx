a8edd6ebe4615200ad0e4e7303fffd3fc6f4436c
Al Miha <contact.almiha@gmail.com>
2025-03-13 16:41:03 +0100

    174+175+176.Handling Form Submission

--- frontend-react/src/components/dashboard/Dashboard.jsx
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -1,21 +1,54 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import axiosInstance from "../../axiosinstance";
 
 const Dashboard = () => {
+  const [ticker, setTicker] = useState("");
   // const accessToken = localStorage.getItem("accessToken");
   useEffect(() => {
     const fetchProtectedData = async () => {
       try {
         const response = await axiosInstance.get("/protected-view");
-        console.log("Success: ", response.data);
+        // console.log("Success: ", response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
     fetchProtectedData();
   }, []);
-  return <div className="text-light container">Dashboard</div>;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axiosInstance.post("/predict/", {
+        ticker: ticker,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error("There was an error making the API request", error);
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 mx-auto">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Stock Ticker"
+              onChange={(e) => setTicker(e.target.value)}
+              required
+            />
+            <button type="submit" className="btn btn-info mt-3">
+              See Prediction
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
